c4c0a041dad2a78ecc762d187e1e2fa5693a07f2
fine <92whdtjs@gmail.com>
2025-02-20 16:02:21 +0900

    #7-권한체크방식 변경

--- Soundscape.swiftpm/App/ContentView.swift
+++ Soundscape.swiftpm/App/ContentView.swift
@@ -2,14 +2,31 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var vm = ARVisualizationViewModel()
+    @StateObject private var permissionManager = PermissionManager()
+    @State private var isChecking = true
     
     var body: some View {
-        ARVisualizationView(viewModel: vm)
-            .onAppear {
-                vm.startCapture()
+        
+        Group{
+            if isChecking {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+            } else {
+                if permissionManager.hasRequiredPermissions {
+                    ARVisualizationView(viewModel: vm)
+                } else {
+                    PermissionStatusView(permissionManager: permissionManager)
+                }
             }
-            .onDisappear{
-                vm.stopCapture()
-            }
+        }
+        .onAppear {
+            permissionManager.checkPermissionStatus()
+            isChecking = false
+            
+            vm.startCapture()
+        }
+        .onDisappear{
+            vm.stopCapture()
+        }
     }
 }
